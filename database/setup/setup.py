a6a7ba40b844322352d9824d8ece6a37ddbf3775
Bastian Lipka <lipka.bastian04@gmail.com>
2025-06-07 11:16:30 -0700

    load model

--- database/setup/setup.py
+++ database/setup/setup.py
@@ -1,8 +1,14 @@
 import pandas as pd 
-import gensim
+from gensim.models import Word2Vec
 
 def main():
-    ...
+    model =  Word2Vec.load("model/b25-CBOW-256-5-150v2.model")
+    
+    for kv in model.wv:
+        ...
+        # take pair and put it into the vector store
+        # also get the mapping from the database
+        # insert all into postgres pg
 
 if __name__ == '__main__':
     main()

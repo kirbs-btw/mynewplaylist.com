60bae0e389b8a55811115c8ae9a66ff5505b48d8
Bastian Lipka <lipka.bastian04@gmail.com>
2025-07-15 21:19:16 -0700

    fix: inserting data into database on droplet

--- database/setup/insert.sql
+++ database/setup/insert.sql
@@ -1,2 +1,9 @@
+-- need to insert the data into the container first
+-- docker cp /home/lipka/mynewplaylist.com/database/setup/combined_csvsongs_with_embeddings_v3.csv vector-db-prod:/tmp/songs.csv
+-- docker exec -it vector-db-prod psql -U postgres -d vectordemo
+
 SET client_encoding = 'UTF8';
-\COPY b25.songs(track_id,track_name,track_external_urls,artist_name,embedding) FROM 'C:\Users\lipka\HARD_DRIVE\Projekte\Projekte\Project 146\mynewplaylist.com\database\setup\combined_csv\songs_with_embeddings_v3.csv' DELIMITER ',' CSV HEADER;
+\COPY b25.songs(track_id,track_name,track_external_urls,artist_name,embedding) FROM '/tmp/songs.csv' DELIMITER ',' CSV HEADER;
+
+-- docker exec -it vector-db-prod /bin/bash
+-- rm -r /tmp/songs.csv
